8ce57267be951e67a804efbe4a8e82fa6bced953
Jakub Stasiak <jakub@stasiak.at>
2016-06-11 13:35:24 +0200

    Only support Python 3.5 from now on
    coverage, which we intend to use in testing, is not compatible with
    Python 3.0-3.2 and Python <= 3.2 is not really relevant these days anyway.
    
    We intend to use the selectors[1] module but that's in the Python
    standard library only starting with Python 3.4 and only in Python 3.5
    select() methods got fixed to retry when interrupted by a signal[2] (on
    3.4 it'd just return an empty list).
    
    [1] https://docs.python.org/3/library/selectors.html
    [2] https://docs.python.org/3/library/selectors.html#selectors.BaseSelector.select

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ except:
 platform = get_platform()
 
 # check Python's version
-if sys.version_info < (3, 0):
-    sys.stderr.write('This module requires at least Python 3.0\n')
+if sys.version_info < (3, 5):
+    sys.stderr.write('This module requires at least Python 3.5\n')
     sys.exit(1)
 
 classif = [
@@ -33,11 +33,7 @@ classif = [
     'Operating System :: POSIX :: Linux',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.0',
-    'Programming Language :: Python :: 3.1',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: System :: Filesystems',
